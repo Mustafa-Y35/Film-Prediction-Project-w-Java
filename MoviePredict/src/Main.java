533d78c1764c344b6569a695a9ecc75c2dfe61cd
Mustafa Yilmaz <mustafaylmz201015@gmail.com>
2023-06-21 21:37:44 +0300

    new

--- MoviePredict/src/Main.java
+++ MoviePredict/src/Main.java
@@ -1,11 +1,11 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        String csvFile = "dosya.csv";
+    public static void main(String[] args){
+        String csvFile = "C:\\Users\\musta\\OneDrive\\Belgeler\\GitHub\\proje\\MoviePredict\\src\\imdb_top_250.csv";
         String line;
         String csvSplitBy = ";";
 
@@ -15,16 +15,16 @@ public class Main {
             while ((line = br.readLine()) != null) {
                 String[] movieData = line.split(csvSplitBy);
                 if (movieData.length == 8) {
-                    int no = Integer.parseInt(movieData[0]);
+                    String no = movieData[0];
                     String title = movieData[1];
-                    int year = Integer.parseInt(movieData[2]);
+                    String year = movieData[2];
                     String genre = movieData[3];
                     String origin = movieData[4];
                     String director = movieData[5];
                     String star = movieData[6];
                     String imdbLink = movieData[7];
 
-                    Movie movie = new Movie(no, title,year, genre, origin, director, star, imdbLink);
+                    Movie movie = new Movie(no,title,year,genre,origin,director,star,imdbLink);
                     movies.add(movie);
                 } else {
                     System.out.println("Hatalı satır: " + line);
@@ -34,56 +34,48 @@ public class Main {
             e.printStackTrace();
         }
 
-        // Depolanan verilerle ilgili işlemleri burada yapabilirsiniz
         for (Movie movie : movies) {
+            System.out.println(movie.getNo());
             System.out.println(movie.getTitle());
+            System.out.println(movie.getYear());
+            System.out.println(movie.getGenre());
+            System.out.println(movie.getOrigin());
+            System.out.println(movie.getDirector());
+            System.out.println(movie.getStar());
+            System.out.println(movie.getLink());
+        }
+
+        System.out.println("\n");
+
+        int lineNumber = 0;
+
+        try {
+            FileReader fileReader = new FileReader(csvFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((bufferedReader.readLine()) != null) {
+                lineNumber++;
+            }
+
+            Random random = new Random();
+            int randomLineNumber = random.nextInt(lineNumber);
+
+            fileReader = new FileReader(csvFile);
+            bufferedReader = new BufferedReader(fileReader);
+            lineNumber = 0;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                if (lineNumber == randomLineNumber) {
+                    System.out.println(line);
+                    break;
+                }
+                lineNumber++;
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
         }
     }
 }
-class Movie {
-    private int no,year;
-    private String title,genre,origin,director,star,link;
 
-    public Movie(int no, int year, String title, String genre, String origin, String director, String star, String link) {
-        this.no = no;
-        this.year = year;
-        this.title = title;
-        this.genre = genre;
-        this.origin = origin;
-        this.director = director;
-        this.star = star;
-        this.link = link;
-    }
-
-    public int getNo() {
-        return no;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public String getStar() {
-        return star;
-    }
-
-    public String getLink() {
-        return link;
-    }
-}
